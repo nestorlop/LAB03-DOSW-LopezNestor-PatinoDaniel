9b58aafc996d37582403cb0011e5aecb5aad60b8
daniel-pm19 <daniel.patino-m@mail.escuelaing.edu.co>
2025-08-28 23:38:21 -0500

    Parte 1 -Daniel: Se creó la estructura del proyecto maven, incluyendo archivo .gitignore y ramas

---
+++ src/test/java/edu/dosw/lab/LAB03_DOSW_LopezNestor_PatinoDaniel/Lab03DoswLopezNestorPatinoDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.LAB03_DOSW_LopezNestor_PatinoDaniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03DoswLopezNestorPatinoDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
