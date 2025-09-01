b686196538e2ae19c02e3822714f2810b05894f1
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-09-01 18:11:34 -0500

    Clases iniciales reto 3

--- src/main/java/edu/dosw/lab/LAB03_DOSW_LopezNestor_PatinoDaniel/Simulacion/Votos.java
+++ src/main/java/edu/dosw/lab/LAB03_DOSW_LopezNestor_PatinoDaniel/Simulacion/Votos.java
@@ -1,5 +1,27 @@
 package edu.dosw.lab.LAB03_DOSW_LopezNestor_PatinoDaniel.Simulacion;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+
 public class Votos {
-    
+    public ArrayList<String> miembros = new ArrayList<>();
+    public Votos(ArrayList miembros){
+        this.miembros = miembros;
+    }
+    public ArrayList getMiembros(){
+        return miembros;
+    }
+     public boolean resultados(Map<String,Integer> votos){
+     if (votos.isEmpty()) {
+        return false;
+     }
+     Integer resultado = votos.values().iterator().next();  
+     for ( Integer i : votos.values()) {
+        if(!Objects.equals(resultado, i)){
+            return false;
+        }
+     }
+     return true;
+    }
 }
